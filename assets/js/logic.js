25d1450321b18044fec33d3384b0a72f6c843d38
Andzej-S <andrewstreckul@gmail.com>
2023-01-13 21:45:56 +0000

    add display question function that has choices input

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,19 @@
 const startQuizBtn = document.getElementById("start");
 const startScreen = document.querySelector(".start");
 const endScreen = document.getElementById("end-screen");
+const questionEl = document.getElementById("question-title");
+const questionWrapperEl = document.getElementById("questions");
+const choicesEl = document.getElementById("choices");
+
+let currentQuestion = 0;
+
 
 // Function to hide whole start class to make space for the quizQuestions
 const hideStart = () => {
     startScreen.classList.remove("start")
     startScreen.classList.add("hide");
 };
+
 
 // Function to start a timer
 const startTimer = () => {
@@ -35,7 +42,29 @@ const startTimer = () => {
     const intervalId = setInterval(updateTimer, 1000);
 };
 
+
+// Function to display questions and choices
+const displayQuestion = () => {
+    for(let i = 0; i < quizQuestions.length; i++) {
+        if(i === currentQuestion) {
+
+            // Un-hide the elements that display question and choices
+            questionWrapperEl.classList.remove("hide");
+            questionEl.innerHTML = quizQuestions[i].question;
+            choicesEl.innerHTML = "";
+
+            // Shows all the choices for a particular question
+            quizQuestions[i].choices.map((choice, index) => {
+                choicesEl.innerHTML += `<input type="radio" name="answer" value="${choice}"> ${index + 1}. ${choice}<br>`;
+            });
+        }
+    }
+}
+
+
+// Event listener for when the user clicks on Start Quiz button
 startQuizBtn.addEventListener("click", () => {
     startTimer();
     hideStart();
+    displayQuestion();
 });
