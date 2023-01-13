81029e3b019c4e9fb9ac3aa1617003b6c462a045
Andzej-S <andrewstreckul@gmail.com>
2023-01-13 11:14:37 +0000

    add array of objects containing questions, choices and correct answers

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+const quizQuestions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ________",
+        choices: ["quotes", "curcly brackets", "parentheses", "square brackets" ],
+        correctAnswer: "parentheses"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store:",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correctAnswer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        correctAnswer: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        correctAnswer: "console.log"
+    }
+];
